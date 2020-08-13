3854fcd6935b9b44da2f6c4230df30ecbe4a02d1
TookTalk2Mac <wootaejung24@gmail.com>
2020-08-14 00:39:58 +0900

    JS : Arrow up

--- main.js
+++ main.js
@@ -38,6 +38,19 @@ document.addEventListener('scroll', () => {
   content.style.opacity = (homeHeight - window.scrollY) / homeHeight;
 });
 
+// Show Arrow up Button when scrolling down
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrowUp.classList.add('visible');
+  } else {
+    arrowUp.classList.remove('visible');
+  }
+});
+arrowUp.addEventListener('click', () => {
+  scrollIntoView('#home');
+});
+
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: 'smooth' });
