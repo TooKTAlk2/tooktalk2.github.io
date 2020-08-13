b7b146d7edcf99c9cef96053e29d6e33d43c9ccf
TookTalk2Mac <wootaejung24@gmail.com>
2020-08-13 10:59:56 +0900

    navbar transparents  and  handle scrolling when navbar

---
+++ main.js
@@ -0,0 +1,26 @@
+'use strict';
+
+// make navbar transparent when it is on the top, otherwise it is pink background-color
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+
+document.addEventListener('scroll', () => {
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add('navbar--dark');
+  } else {
+    navbar.classList.remove('navbar--dark');
+  }
+});
+// handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+
+navbarMenu.addEventListener('click', (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null || link == undefined) {
+    return;
+  }
+  console.log(event.target.dataset.link);
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: 'smooth' });
+});
